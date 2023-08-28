cb2840b9ff0738d444d9d005094a1d7fba1b47f2
Pujan Deb Nath <120366627+Pujan-deb@users.noreply.github.com>
2023-08-28 20:51:32 +0600

    Elephant.cpp

---
+++ Elephant.cpp
@@ -0,0 +1,27 @@
+//Problem link: https://codeforces.com/contest/617/problem/A
+/*
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+Input
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+
+Examples
+inputCopy
+5
+outputCopy
+1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout << (n+4)/5;
+    return 0;
+ 
+}
