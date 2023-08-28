8746a315b7696d09f72de34ab94d44252a189cf2
Pujan Deb Nath <120366627+Pujan-deb@users.noreply.github.com>
2023-08-28 13:43:53 +0600

    Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,17 @@
+//Problem Link: https://codeforces.com/contest/4/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n==2){
+        cout << "NO";
+    }else if(n%2==0){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+ 
+    return 0;
+}
